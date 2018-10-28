e6c00f0d8c6af9a2fde98da5faeeac7af6696394
John Washam <johndubyamo@gmail.com>
2018-10-27 21:48:35 -0500

    Proof of concept

--- SkiaSharpSandbox/Form1.Designer.cs
+++ SkiaSharpSandbox/Form1.Designer.cs
@@ -28,13 +28,85 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.xPos = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.yPos = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.skiaImage = new System.Windows.Forms.PictureBox();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.skiaImage)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // xPos
+            // 
+            resources.ApplyResources(this.xPos, "xPos");
+            this.xPos.Name = "xPos";
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.Name = "label2";
+            // 
+            // yPos
+            // 
+            resources.ApplyResources(this.yPos, "yPos");
+            this.yPos.Name = "yPos";
+            // 
+            // button1
+            // 
+            resources.ApplyResources(this.button1, "button1");
+            this.button1.Name = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.onClickRenderBtn);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button1);
+            resources.ApplyResources(this.groupBox1, "groupBox1");
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.TabStop = false;
+            // 
+            // skiaImage
+            // 
+            resources.ApplyResources(this.skiaImage, "skiaImage");
+            this.skiaImage.Name = "skiaImage";
+            this.skiaImage.TabStop = false;
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.skiaImage);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.yPos);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.xPos);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.skiaImage)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox xPos;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox yPos;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.PictureBox skiaImage;
     }
 }
 
--- SkiaSharpSandbox/Form1.cs
+++ SkiaSharpSandbox/Form1.cs
@@ -1,9 +1,12 @@
-﻿using System;
+﻿using SkiaSharp;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -15,6 +18,82 @@ namespace SkiaSharpSandbox
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void onClickRenderBtn(object sender, EventArgs e)
+        {
+            string xText = xPos.Text,
+                yText = yPos.Text;
+            float x, y;
+            try
+            {
+                float.TryParse(xText, out x);
+                float.TryParse(yText, out y);
+
+                SKImageInfo imageInfo = new SKImageInfo(300, 250);
+
+                using (SKSurface surface = SKSurface.Create(imageInfo))
+                {
+                    SKCanvas canvas = surface.Canvas;
+
+                    canvas.Clear(SKColors.Red);
+
+                    //set up drawing tools
+                    using (SKPaint paint = new SKPaint())
+                    {
+                        paint.IsAntialias = true;
+                        paint.Color = new SKColor(0x2c, 0x3e, 0x50);
+                        paint.StrokeCap = SKStrokeCap.Round;
+
+                        //create the Xamagon path
+                        using (SKPath path = new SKPath())
+                        {
+                            path.MoveTo(71.4311121f, 56f);
+                            path.CubicTo(68.6763107f, 56.0058575f, 65.9796704f, 57.5737917f, 64.5928855f, 59.965729f);
+                            path.LineTo(43.0238921f, 97.5342563f);
+                            path.CubicTo(41.6587026f, 99.9325978f, 41.6587026f, 103.067402f, 43.0238921f, 105.465744f);
+                            path.LineTo(64.5928855f, 143.034271f);
+                            path.CubicTo(65.9798162f, 145.426228f, 68.6763107f, 146.994582f, 71.4311121f, 147f);
+                            path.LineTo(114.568946f, 147f);
+                            path.CubicTo(117.323748f, 146.994143f, 120.020241f, 145.426228f, 121.407172f, 143.034271f);
+                            path.LineTo(142.976161f, 105.465744f);
+                            path.CubicTo(144.34135f, 103.067402f, 144.341209f, 99.9325978f, 142.976161f, 97.5342563f);
+                            path.LineTo(121.407172f, 59.965729f);
+                            path.CubicTo(120.020241f, 57.5737917f, 117.323748f, 56.0054182f, 114.568946f, 56f);
+                            path.LineTo(71.4311121f, 56f);
+                            path.Close();
+
+                            //draw the Xamagon path
+                            canvas.DrawPath(path, paint);
+                        }
+                    }
+
+                    using (WebClient client = new WebClient())
+                    using (Stream stream = client.OpenRead("https://via.placeholder.com/200x100"))
+                    {
+                        SKBitmap bitmap = SKBitmap.Decode(stream);
+                        canvas.DrawBitmap(bitmap, SKRect.Create(30, 30, 200, 100));
+                    }
+
+                    using (SKPaint textPaint = new SKPaint() { Color = SKColor.Parse("0000cc"), IsAntialias = true, TextSize = 24 })
+                    using (SKTypeface tf = SKTypeface.FromFamilyName("Courier New"))
+                    {
+                        canvas.DrawText("Test", 30, 30, textPaint); //x and y are baseline
+                    }
+
+                    using (SKImage image = surface.Snapshot())
+                    using (SKData data = image.Encode(SKEncodedImageFormat.Png, 100))
+                    using (MemoryStream mStream = new MemoryStream(data.ToArray()))
+                    {
+                        Bitmap bm = new Bitmap(mStream, false);
+                        skiaImage.Image = bm;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
         }
     }
 }
